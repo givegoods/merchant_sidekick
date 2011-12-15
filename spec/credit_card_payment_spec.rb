6a6821e5aa27188b77e236979cbb9e08e34303b4
Norman Clarke <norman@njclarke.com>
2011-12-15 11:04:56 -0300

    Got credit card payment spec running

--- spec/credit_card_payment_spec.rb
+++ spec/credit_card_payment_spec.rb
@@ -1,4 +1,6 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.expand_path("../spec_helper", __FILE__)
+
+CreditCardPayment = MerchantSidekick::Payments::CreditCardPayment
 
 describe CreditCardPayment, "authorization" do
   
